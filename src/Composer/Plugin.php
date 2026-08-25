59244f769bf99fde3057e596520127bcc56b86c8
Jascha030 <contact@jaschavanaalst.nl>
2026-08-25 14:11:42 +0200

    ✨ composer: Add Composer plugin command and local template source

---
+++ src/Composer/Plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the jascha030/composer-scaffolder package.
+ *
+ * (c) Jascha van Aalst <contact@jaschavanaalst.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Jascha030\Scaffolder\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\Capable;
+use Composer\Plugin\PluginInterface;
+
+final class Plugin implements PluginInterface, Capable
+{
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function getCapabilities(): array
+    {
+        return [
+            \Composer\Plugin\Capability\CommandProvider::class => CommandProvider::class,
+        ];
+    }
+}
